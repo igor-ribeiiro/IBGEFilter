5053d5b53ed34d6e36a85b653f12db7f9b847ac3
Igor_Ribeiro <ribeiro_igor@hotmail.com>
2017-05-23 01:37:56 -0300

    Putting files to a public folder

--- public/js/index.js
+++ public/js/index.js
@@ -3,6 +3,6 @@ import ReactDOM from 'react-dom';
 
 import App from './components/App.jsx';
 
-console.log('hahah');
 
-ReactDOM.render(<App />, document.getElementById('app'));
+ReactDOM.render(<App />, 
+	document.getElementById('app'));
