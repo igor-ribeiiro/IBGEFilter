4a44dbfe64cc17d8e675885bcbdc3eeb22c7ac55
luisholanda <luiscmholanda@gmail.com>
2017-05-23 16:47:42 -0300

    Enabled debug

--- settings.py
+++ settings.py
@@ -2,5 +2,6 @@ import os
 
 settings = {
     'template_path': os.path.join(os.path.dirname(__file__), 'views'),
-    'static_path': os.path.join(os.path.dirname(__file__), 'public')
+    'static_path': os.path.join(os.path.dirname(__file__), 'public'),
+    'debug': True
 }
