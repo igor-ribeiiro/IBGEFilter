2109ce312576c87c47ec60fee7d72f6e0b76d9d2
luisholanda <luiscmholanda@gmail.com>
2017-05-24 21:56:56 -0300

    Fixed Typo

--- public/js/components/Heading.jsx
+++ public/js/components/Heading.jsx
@@ -12,7 +12,7 @@ class Heading extends React.Component {
     render() {
         return (
             <div>
-                <h1> Senso do IBGE </h1>
+                <h1> Censo do IBGE </h1>
                 <h4> Dados sobre a população brasileira  de acordo com o senso realizado pelo IBGE no ano 2010</h4>
             </div>
         )
